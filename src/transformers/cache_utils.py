1c74669bdc3654fe56e5962488b9f74eea3d3fe8
knagrecha <32966638+knagrecha@users.noreply.github.com>
2023-12-13 10:55:58 -0800

    Update cache_utils.py

--- src/transformers/cache_utils.py
+++ src/transformers/cache_utils.py
@@ -1,6 +1,7 @@
 from typing import Any, Dict, List, Optional, Tuple
 
 import torch
+import torch.nn.functional as F
 
 
 class Cache:
@@ -52,6 +53,175 @@ class Cache:
         if max_length is not None and previous_seq_length + new_seq_length > max_length:
             return max_length - new_seq_length
         return previous_seq_length
+
+"""
+    Basic implementation for Quantization
+"""
+
+import torch
+
+class Quantizer:
+
+    @staticmethod
+    def pack_bits(tensor: torch.Tensor, bits: int) -> torch.Tensor:
+        """
+        Packs multiple n-bit values into single 8-bit integers.
+        """
+        # Reshape the tensor to a 1D tensor
+        tensor = tensor.view(-1)
+
+        # Initialize the packed tensor
+        packed = torch.zeros((len(tensor) + 1) // 2, dtype=torch.uint8)
+
+        # Pack the tensor
+        packed[::2] = tensor[::2]
+        packed[1::2] = tensor[1::2] << bits
+
+        return packed
+
+    @staticmethod
+    def fp16_to_nbit(tensor: torch.Tensor, bits: int) -> torch.Tensor:
+        """
+        Quantizes a FP16 tensor to n-bit, and packs the values into 8-bit integers.
+        """
+        scale = tensor.abs().max() / (2 ** bits - 1)
+        zero_point = 0  # assuming input tensor is non-negative
+
+        # Quantize the tensor
+        tensor = torch.quantize_per_tensor(tensor, scale, zero_point, torch.quint8)
+
+        # Pack the tensor
+        tensor = Quantizer.pack_bits(tensor, bits)
+
+        return tensor
+
+    @staticmethod
+    def nbits_to_fp16(tensor: torch.Tensor, bits: int) -> torch.Tensor:
+        """
+        Unpacks values from 8-bit integers, dequantizes a n-bit tensor to FP16 for each tensor in a batch in parallel.
+        """
+   
+        tensor = tensor.type(torch.cuda.ByteTensor)
+       
+        # Unpack the tensor
+        tensor = Quantizer.unpack_bits(tensor, bits)
+    
+        # Dequantize the tensor
+        tensor = tensor.dequantize()
+    
+        return tensor
+
+
+class HierarchicalKVCache(Cache):
+    """
+    A cache that grows dynamically as more tokens are generated. This is the default for generative models.
+
+    It stores the Key and Value states as a list of tensors, one for each layer. The expected shape for each tensor is
+    `[batch_size, num_heads, seq_len, head_dim]`.
+    """
+
+    def __init__(self) -> None:
+        self.key_cache: List[List[torch.Tensor]] = []
+        self.value_cache: List[List[torch.Tensor]] = []
+        self.seen_tokens = 0  # Used in `generate` to keep tally of how many tokens the cache has seen
+
+    def __getitem__(self, layer_idx: int) -> List[Tuple[torch.Tensor]]:
+        """
+        Support for backwards-compatible `past_key_value` indexing, e.g. `past_key_value[0][0].shape[2]` to get the
+        sequence length.
+        """
+        if layer_idx < len(self):
+            return (self.key_cache[layer_idx], self.value_cache[layer_idx])
+        else:
+            raise KeyError(f"Cache only has {len(self)} layers, attempted to access layer with index {layer_idx}")
+
+    def __iter__(self):
+        """
+        Support for backwards-compatible `past_key_value` iteration, e.g. `for x in past_key_value:` to iterate over
+        keys and values
+        """
+        for layer_idx in range(len(self)):
+            yield (self.key_cache[layer_idx], self.value_cache[layer_idx])
+
+    def __len__(self):
+        """
+        Support for backwards-compatible `past_key_value` length, e.g. `len(past_key_value)`. This value corresponds
+        to the number of layers in the model.
+        """
+        return len(self.key_cache)
+        
+    
+    def update(
+        self,
+        key_states: torch.Tensor,
+        value_states: torch.Tensor,
+        layer_idx: int,
+        cache_kwargs: Optional[Dict[str, Any]] = None,
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        Updates the cache with the new `key_states` and `value_states` for the layer `layer_idx`.
+
+        Parameters:
+            key_states (`torch.Tensor`):
+                The new key states to cache.
+            value_states (`torch.Tensor`):
+                The new value states to cache.
+            layer_idx (`int`):
+                The index of the layer to cache the states for.
+            cache_kwargs (`Dict[str, Any]`, `optional`):
+                Additional arguments for the cache subclass. No additional arguments are used in `DynamicCache`.
+
+        Return:
+            A tuple containing the updated key and value states.
+        """
+        # Update the number of seen tokens
+        if layer_idx == 0:
+            self.seen_tokens += key_states.shape[-2]
+
+        # Update the cache
+        if len(self.key_cache) <= layer_idx:
+            self.key_cache.append(Quantizer.fp16_to_nbit(key_states, 8))
+            self.value_cache.append(Quantizer.fp16_to_nbit(value_states, 8))
+        else:
+            self.key_cache[layer_idx] = torch.cat([self.key_cache[layer_idx], Quantizer.fp16_to_nbit(key_states, 8)], dim=-2)
+            self.value_cache[layer_idx] = torch.cat([self.value_cache[layer_idx], Quantizer.fp16_to_nbit(value_states, 8)], dim=-2)
+
+        return Quantizer.nbit_to_fp16(self.key_cache[layer_idx]), Quantizer.nbit_to_fp16(self.value_cache[layer_idx])
+
+    def get_seq_length(self, layer_idx: Optional[int] = 0) -> int:
+        """Returns the sequence length of the cached states. A layer index can be optionally passed."""
+        if len(self.key_cache) <= layer_idx:
+            return 0
+        return self.key_cache[layer_idx].shape[-2]
+
+    def get_max_length(self) -> Optional[int]:
+        """Returns the maximum sequence length of the cached states. DynamicCache does not have a maximum length."""
+        return None
+
+    def reorder_cache(self, beam_idx: torch.LongTensor):
+        """Reorders the cache for beam search, given the selected beam indices."""
+        for layer_idx in range(len(self.key_cache)):
+            device = self.key_cache[layer_idx].device
+            self.key_cache[layer_idx] = self.key_cache[layer_idx].index_select(0, beam_idx.to(device))
+            device = self.value_cache[layer_idx].device
+            self.value_cache[layer_idx] = self.value_cache[layer_idx].index_select(0, beam_idx.to(device))
+
+    def to_legacy_cache(self) -> Tuple[Tuple[torch.Tensor], Tuple[torch.Tensor]]:
+        """Converts the `DynamicCache` instance into the its equivalent in the legacy cache format."""
+        legacy_cache = ()
+        for layer_idx in range(len(self)):
+            legacy_cache += ((self.key_cache[layer_idx], self.value_cache[layer_idx]),)
+        return legacy_cache
+
+    @classmethod
+    def from_legacy_cache(cls, past_key_values: Optional[Tuple[Tuple[torch.FloatTensor]]] = None) -> "DynamicCache":
+        """Converts a cache in the legacy cache format into an equivalent `DynamicCache`."""
+        cache = cls()
+        if past_key_values is not None:
+            for layer_idx in range(len(past_key_values)):
+                key_states, value_states = past_key_values[layer_idx]
+                cache.update(key_states, value_states, layer_idx)
+        return cache
 
 
 class DynamicCache(Cache):
